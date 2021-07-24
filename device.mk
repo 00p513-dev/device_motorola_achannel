5d1c834be331d01627eb1ebcbd832b67f483e601
Amy Grace <amy@blisslabs.org>
2021-07-24 13:41:43 +0000

    achannel: Set screen density

--- device.mk
+++ device.mk
@@ -7,6 +7,11 @@
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+TARGET_SCREEN_DENSITY := 340
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
