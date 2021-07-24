78d893c5d976d348815b5020888c77e4da2d640b
Amy Grace <amy@blisslabs.org>
2021-07-24 13:50:46 +0000

    Add platform info

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,3 +5,14 @@
 #
 
 DEVICE_PATH := device/motorola/achannel.mk
+
+# Binder
+TARGET_USES_64_BIT_BINDER := true
+
+# CPU
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a15
+ARCH_ARM_HAVE_TLS_REGISTER := true
