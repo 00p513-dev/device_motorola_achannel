2778237eea6fd8b4550ca65801ae6e9da6a09846
Amy Grace <amy@blisslabs.org>
2021-07-24 13:54:03 +0000

    achannel: Add some bliss specific flags

--- bliss_achannel.mk
+++ bliss_achannel.mk
@@ -21,3 +21,7 @@ PRODUCT_MODEL := moto g⁷ play
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Bliss flags
+TARGET_BOOT_ANIMATION_RES := 720
+BLISS_BUILD_VARIANT := vanilla
